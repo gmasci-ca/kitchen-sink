32eeb7eeca57676d65af57dedb1910ccebbcedfe
Giorgio Masci <gmasci@codearchitects.com>
2020-10-13 15:01:39 +0200

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kitchen-sink';
+  title = 'kitchen-sink2';
+  prop = 3
 }
